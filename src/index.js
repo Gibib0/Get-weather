f0f05b955ccd4bd42fc849ab9dc0fd4842fde80b
Anastasia <anast111antonova@gmail.com>
2025-10-23 21:27:43 +0300

    Add name/id fields checking; add api key

--- src/index.js
+++ src/index.js
@@ -11,3 +11,19 @@ const vancelbtn = document.getElementById('cancelbtn')
 const temperature = document.getElementById('temperature')
 const wind = document.getElementById('wind')
 const humidity = document.getElementById('humidity')
+
+const api = '8df86b370cc7920a7229450f28f03a5d'
+
+radios.forEach(radio => {
+	radio.addEventListener('change', () => {
+		if (radio.value === 'name') {
+			cityNameInput.disabled = false
+			cityIdInput.disabled = true
+			cityIdInput.value = ''
+		} else if (radio.value === 'id') {
+			cityNameInput.disabled = true
+			cityIdInput.disabled = false
+			cityNameInput.value = ''
+		}
+	})
+})
