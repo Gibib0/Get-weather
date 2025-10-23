0969fef5dbd44dab34231faa2b889f49526d1392
Anastasia <anast111antonova@gmail.com>
2025-10-23 21:19:15 +0300

    Initilize html elements in js

---
+++ src/index.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const cityNameInput = document.getElementById('cityName')
+const cityIdInput = document.getElementById('cityId')
+const radios = document.getElementsByName('searchType')
+
+const okbtn = document.getElementById('okbtn')
+const weatherbtn = document.getElementById('weatherbtn')
+const vancelbtn = document.getElementById('cancelbtn')
+
+const temperature = document.getElementById('temperature')
+const wind = document.getElementById('wind')
+const humidity = document.getElementById('humidity')
